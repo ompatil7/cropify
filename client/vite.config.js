2d90ce77a40f13d286fa5841e3f3b01ec5f47b63
ompatil7 <ompatil4749@gmail.com>
2024-07-09 22:57:23 +0530

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,20 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
+
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://cropify-server.vercel.app/",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+// // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
@@ -13,5 +30,10 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      "@emotion/react": "@emotion/react",
+      "@emotion/styled": "@emotion/styled",
+    },
+  },
 });
-// vite.config.js
